2405be8713c7da8562fe3f064deb340253dab01d
wisniem7 <marta.wisniewska19.10@gmail.com>
2015-01-04 11:31:09 +0100

    Zawiera funkcje do obliczenia a0, a, b dla danych punktow

---
+++ aproksymator_f_trygonometrycznymi.h
@@ -0,0 +1,12 @@
+#ifndef APROKSYMATOR_F_TRYGONOMETRYCZNYMI_H
+#define APROKSYMATOR_F_TRYGONOMETRYCZNYMI_H
+
+#include "punkty.h"
+#include "funkcje_trygonometryczne.h"
+#include <math.h>
+
+
+void  oblicz_parametry ( punkty_t *pts, parametry_funkcji_t *par);
+
+#endif
+
